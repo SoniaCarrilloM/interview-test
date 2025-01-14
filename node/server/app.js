168d2a53eb0e3c07eb499fe7fc17c7b255f6d74b
SoniaCarrilloM <154975385+SoniaCarrilloM@users.noreply.github.com>
2025-01-14 22:28:27 +0100

    updated test.js

--- node/server/app.js
+++ node/server/app.js
@@ -6,19 +6,18 @@ const teacherRoutes = require("./teacherRoutes");
 const studentRoutes = require("./studentRoutes");
 
 const app = express();
-const port = process.env.PORT || 3001; //
+const port = process.env.PORT || 3002; //
 
 // Middleware
-app.use(cors({ origin: "http://localhost:3003" })); // Add this line to allow requests from the frontend's origin
+app.use(cors({ origin: "http://localhost:3002" })); // Add this line to allow requests from the frontend's origin
 app.use(bodyParser.json());
 app.listen(3001, () => {
   console.log("Server is running on port 3001");
 
-// Register routes
-assignmentRoutes(app);
-teacherRoutes(app);
-studentRoutes(app);
-
+  // Register routes
+  assignmentRoutes(app);
+  teacherRoutes(app);
+  studentRoutes(app);
 });
 
 module.exports = app;
